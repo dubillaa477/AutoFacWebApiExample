667efad6a608bf647aec9c3debbae0a5eacdc7bf
Daniel Ubilla Acuña <dubillaa477@ulacit.ed.cr>
2018-12-03 19:18:42 -0600

    Add project files.

---
+++ AutoFacWebExample/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AutoFacWebExample.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AutoFacWebExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoFacWebExample.WebApiApplication" Language="C#" %>
